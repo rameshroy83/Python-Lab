07d41e87af4ce77aa24e7a84d79577ef7875cf9e
rameshroy83 <rameshroy83@gmail.com>
2020-10-04 07:46:00 -0400

    TEst

--- Server-Socket-Lab1.py
+++ Server-Socket-Lab1.py
@@ -14,18 +14,19 @@ port = 4444
 
 ADDR = (host, port)
 #Now we will create a binding of the IPv4 and Port
-serversocket.bind((host, port))
-#serversocket.bind(ADDR) alternative method as bind takes single parameter.
+#serversocket.bind((host, port))
 
+serversocket.bind(ADDR)
+
+#We are defining different functions for Server, Client handling.
 #Now we will set the server listern, also we can define number of connections to accept.
 #serversocket.listen(3)
 
 
-def handle_client(conn, addr):
-    pass
-
+# This function is for server handling.
 def start():
     serversocket.listen()
+    # Once the socket is binded we are making the server to listen for connection request.
 
     while True:
         conn, addr = serversocket.accept()
@@ -33,6 +34,21 @@ def start():
         #and also it stores it as an object conn so that we can sent info back to the coneection from client.
         # the addr is actually a tuple which gives the port and ip address from where the connection was established.
         
+        #Now here the purpose of thread is to count the number of active connections.
+        #Each client handling would be done as a seperate thread.
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"Active Connections = {threading.activeCount() - 1}")
+
+
+def handle_client(conn, addr):
+    #This function would run for each client
+    print(f"New Connection = {addr} connected")
+
+    connected = True
+
+    while connected:
+
 
 
 
